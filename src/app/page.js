101c365119cd8a1061dd0562fa70ed1ae1711095
RIMqanvus <141220989+RIMqanvus@users.noreply.github.com>
2023-11-04 12:49:26 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import Footer from './components/Footer'
 
 export default function Home() {
   return (
-    // <main className="flex min-h-screen flex-col bg-[#121212]">
+    <main className="flex min-h-screen flex-col bg-[#121212]">
     <>
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
@@ -19,6 +19,6 @@ export default function Home() {
         <Footer />
       </div>
     </>
-    // </main>
+   </main>
   )
 }
